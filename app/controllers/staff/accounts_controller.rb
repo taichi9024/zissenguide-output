374677fcc5b6509b77558c22f80d9d1375f49852
Taichi Noguchi <69791626+taichi9024@users.noreply.github.com>
2020-11-30 17:20:36 +0900

    finish customer-route

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -1,20 +1,33 @@
 class Staff::AccountsController < Staff::Base
-    before_action :staff_member_set
+    before_action :set_staff
+
     def show
+        @staff_member = @current_staff_member
     end
 
     def edit
+        @staff_member = @current_staff_member
     end
 
     def update
+        @staff_member = @current_staff_member
+        if @staffmember.update(staff_params)
+            flash.now.notice = "情報を更新しました"
+            redirect_to :staff_account
+        elseß
+            render :edit
+        end
     end
 
         private 
-        def staff_member_set
-          if current_staff_member
-            @staff_member = @current_staff_member
-          else
-            @current_staff_member = nil
-          end
+
+        def set_staff
+            if current_staff_member
+              @staff_member = @current_staff_member
+            end
+        end
+
+        def staff_params
+            params.require(:staff_member).permit(:email, :family_name, :given_name)
         end
 end
